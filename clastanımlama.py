558370f86e739414fc396de54ac5a1754449752d
EmirAKSOY1 <emiraksoy10@gmail.com>
2022-10-20 14:09:04 +0300

    nesne

---
+++ clastanımlama.py
@@ -0,0 +1,13 @@
+class sinif:
+    a=0
+    b="asdf"
+    def yaz(self):
+        print(self.a)
+
+    def yazdır2(self,t):
+        print(t)
+
+nesne =sinif()
+print(nesne.b)
+nesne.yaz()
+nesne.yazdır2(4)
